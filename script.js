cdfbdb64fc4a31c0a3495bcb5a976e2b17ce62d6
Douglas Mitchell <roxonus@gmail.com>
2020-08-13 20:25:51 -0700

    Artemi Panarin

--- script.js
+++ script.js
@@ -18,19 +18,39 @@ $.ajax(
     $(".venue").append(venue);
 
 
+    //API query for specific team roster
     $.ajax(
       {url: "https://statsapi.web.nhl.com/api/v1/teams/3/roster", method: "GET"}  
     ).then(function(response) {
         console.log(response)
+        var name = response.roster[18].person.fullName;
+        var number = response.roster[18].jerseyNumber;
+        var position = response.roster[18].position.name;
+
+        $(".name").append(name);
+        $(".number").append(number);
+        $(".position").append(position);
+
+        //API query for selecting specific player
+        $.ajax(
+            {url: "https://statsapi.web.nhl.com/api/v1/people/8478550", method: "GET"}
+        ).then(function(response) {
+            console.log(response)
+
+            $.ajax(
+                {url: "https://statsapi.web.nhl.com/api/v1/people/8478550/stats?stats=statsSingleSeason&season=20182019", method: "GET"}  
+              ).then(function(response) {
+                  console.log(response)
+        });
     });
     
 });
 
-//API query for specific team roster
-//https://statsapi.web.nhl.com/api/v1/teams/3/roster
 
-//API query for selecting specific player
-//https://statsapi.web.nhl.com/api/v1/people/8476459
+});
+
+
+
 
 //API endpoint for pulling one specific players stats
 //https://statsapi.web.nhl.com/api/v1/people/8476459/stats?stats=statsSingleSeason&season=20182019
